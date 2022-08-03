8c52a7e1f70827ce2ffebc98309d110f03c19022
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-08-02 23:36:04 -0700

    Add files via upload

---
+++ pip.py
@@ -0,0 +1,8 @@
+# ==> pip  is  the package manageris python & python instalationpackage
+# ==> Devops is the stands for development and oparation
+# ==> Homebrew package manager is ,or macos   as well as linux 
+# ==> pep8 stands forpython enhancement propasal  It is a coding convention guide
+# ==> pep8 defination means is a document that provides guidelines and best practises on how to write python code 
+# ==> simply wecalled it isa coding convention guide
+# ==> pypi.org stands for python package index simply we called python package index 
+# PyPI defination is the default software repository for Python developers to store created Python programming language software developers and programmers alike use to publicize and share their software 
